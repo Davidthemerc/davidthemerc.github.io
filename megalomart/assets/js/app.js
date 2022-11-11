21422e919ffa8ee06afb35f4e10f76de190ed973
Davidthemerc <dmerc90@gmail.com>
2022-11-11 09:22:50 -0800

    General update, linked several unlinked pages, added Mega Lo Mart

---
+++ megalomart/assets/js/app.js
@@ -0,0 +1,5 @@
+const searchButton = document.getElementById('search');
+
+searchButton.addEventListener('click', () => {
+  alert('Sorry, our search function is broken. Our developers are on strike!');
+});
