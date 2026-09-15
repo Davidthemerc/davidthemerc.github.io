45375f32eb9494455397536c6932a5a909e8fc92
David Mercado <dmerc90@gmail.com>
2026-09-14 19:43:42 -0700

    SOS update

--- siegeofshantium/js/core/bootstrap-data.js
+++ siegeofshantium/js/core/bootstrap-data.js
@@ -1,4 +1,4 @@
-const RELEASE_VERSION='1.6.66.8.3';
+const RELEASE_VERSION='1.6.66.8.4';
 const SAVE_SCHEMA_VERSION='1.6.41';
 const VERSION=SAVE_SCHEMA_VERSION;
 const SAVE_KEY='siegeOfShantium.save.v1'; // legacy/current mirror
